70b299861b50f51b1edf218eab9c075e990109f0
Jay Lynch <hi@jaylyn.ch>
2015-03-06 13:58:37 +1100

    Serve assets directory

--- startupweek/urls.py
+++ startupweek/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'startupweek.views.home', name='home'),
     #url(r'', include('homepage.urls')),
     url(r'^$', 'django.contrib.staticfiles.views.serve', kwargs={
-            'path': 'index.html'}),#, 'document_root': settings.STATIC_ROOT}),
+            'path': 'index.html'}),
+    url(r'^(?P<path>.*)', 'django.contrib.staticfiles.views.serve'),
+    #url(r'^assets/', 'django.contrib.staticfiles.views.serve'),#, 'document_root': settings.STATIC_ROOT}),
 )
