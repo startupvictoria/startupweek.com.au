8035ac65a92d6d647a1c681f9f6347115661a5cc
Jay Lynch <hi@jaylyn.ch>
2015-03-07 22:25:00 +1100

    Use Github Pages

--- homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-
-from homepage import views
-
-urlpatterns = patterns('',
-  #url('^$', TemplateView.as_view(template_name='index.html')),
-  url('^$', views.index, name='index'),
-)
